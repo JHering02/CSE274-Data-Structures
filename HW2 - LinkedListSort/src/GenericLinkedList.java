a1b9e8e07f77479baffe39fe47ab43ed476e361b
James H <James H@James-H-LegionY740>
2022-09-23 17:17:55 -0400

    Finished the entire project, everything works as intended.

--- HW2 - LinkedListSort/src/GenericLinkedList.java
+++ HW2 - LinkedListSort/src/GenericLinkedList.java
@@ -7,8 +7,9 @@ import java.util.NoSuchElementException;
  * @author James Hering (CSE 274, 22 September 2022)
  *
  */
-public class GenericLinkedList<T> {
+public class GenericLinkedList<T extends Comparable<T>> {
 	private Node<T> head;
+	private Node<T> tail;
 	private int size = 0;
 
 	/*
@@ -19,27 +20,20 @@ public class GenericLinkedList<T> {
 		size = 0;
 	}
 
-	/*
-	 * Returns a space-separated list of the elements in the list. If the list has
-	 * no elements, return an empty string ("")
-	 */
-	public String toString() {
-		return null;
-	}
-
 	/**
 	 * Method used to add a new Node to the singly LinkedList.
 	 * 
 	 * @param newEntry The new data entry
 	 */
 	public void insert(T newEntry) {
+		Node<T> temp = new Node<T>(newEntry);
+		// Setting the new Node to both if the linked list is empty
 		if (head == null) {
-			this.head = new Node<T>(newEntry);
+			head = temp;
+			tail = temp;
 		} else {
-			Node<T> temp = new Node<T>(head.data);
-			temp.next = head.next;
-			this.head = new Node<T>(newEntry);
-			this.head.next = temp;
+			tail.next = temp;
+			tail = temp;
 		}
 		size++;
 	}
@@ -48,6 +42,7 @@ public class GenericLinkedList<T> {
 	 * Method used to remove the top element from the singly LinkedList.
 	 */
 	public T remove() {
+		// Doesn't remove if the list is empty
 		if (this.size == 0) {
 			throw new NoSuchElementException();
 		} else {
@@ -63,6 +58,7 @@ public class GenericLinkedList<T> {
 	 * IndexOutOfBoundsException.
 	 */
 	public T getFirst() {
+		// Doesn't find anything if the list is empty
 		if (this.size == 0) {
 			throw new NoSuchElementException();
 		} else {
@@ -81,6 +77,7 @@ public class GenericLinkedList<T> {
 		}
 		Node<T> temp = head;
 		int current = 0;
+		// Iterate through the list until the index is found
 		while (current != index) {
 			current++;
 			temp = temp.next;
@@ -94,7 +91,7 @@ public class GenericLinkedList<T> {
 	public int getSize() {
 		return this.size;
 	}
-	
+
 	/**
 	 * Returns whether or not the list is empty.
 	 */
@@ -114,13 +111,82 @@ public class GenericLinkedList<T> {
 		size = 0;
 	}
 
+	/**
+	 * Allows a call to the mergeSort method.
+	 */
+	public void sort() {
+		head = mergeSort(this.head);
+	}
+
+	/**
+	 * Begins the recursive merge sort method in order to try and sort the
+	 * LinkedList.
+	 * 
+	 * @param head beginning of the list
+	 * @return
+	 */
+	private Node<T> mergeSort(Node<T> head) {
+		// Checking if the head doesn't exist or is empty
+		if (head == null || head.next == null) {
+			return head;
+		}
+		// Setting a pivot point
+		int currSize = 0;
+		Node<T> temp = head;
+		while (temp.next != null) {
+			temp = temp.next;
+			currSize++;
+		}
+		int middleLength = currSize / 2;
+		Node<T> mid = head;
+		while (middleLength != 0) {
+			mid = head.next;
+			middleLength--;
+		}
+		// Setting a Second pivot point for the other half of this bound
+		Node<T> nextMid = mid.next;
+		mid.next = null;
+		// Check if the values can even be compared to sort
+		if (Comparable.class.isAssignableFrom(head.data.getClass())) {
+			Node<T> left = mergeSort(head);
+			Node<T> right = mergeSort(nextMid);
+			Node<T> sorted = merge(left, right);
+			return sorted;
+		}
+		// Doesn't return anything if the list cannot be sorted
+		return null;
+	}
+
+	private Node<T> merge(Node<T> start, Node<T> end) {
+		// Declaring a sorted node.
+		Node<T> res = null;
+		// Checking the 2 bounds
+		if (start == null) {
+			return end;
+		}
+		if (end == null) {
+			return start;
+		}
+		// If the 2nd bound is larger, take the first bound
+		if (start.data.compareTo(end.data) <= 0) {
+			res = start;
+			start.next = merge(start.next, end);
+		} else {
+			res = end;
+			res.next = merge(start, end.next);
+		}
+		// Return the sorted nodes for this bound
+		return res;
+
+	}
+
 }
 
 /**
  * A Node class in order to hold generic data types.
  * 
  * @author James Hering
- * @param <T>
+ * @param <T> the generic data type
  */
 class Node<T> {
 	T data;
@@ -131,4 +197,5 @@ class Node<T> {
 		this.data = data;
 		this.next = null;
 	}
+
 }
