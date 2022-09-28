61efd759b657c853245043a7c1ef2340be589931
heringja <heringja@miamioh.edu>
2022-09-28 13:09:58 -0400

    Finished both Queue Interfaces with bugs still included

--- Lab5 - Queues/src/QueueTest.java
+++ Lab5 - Queues/src/QueueTest.java
@@ -6,17 +6,25 @@ import org.junit.Test;
 public class QueueTest {
 	
 	QueueInterface<Integer> queue;
+	QueueInterface<Integer> queue2;
 	
 	@Before
 	public void createQueue() {
 		queue = new ArrayQueue<>(); // change to LinkedQueue when you are ready
+		queue2 = new LinkedQueue<>();
 	}
 
 	@Test
 	public void testIsEmpty() {
+		// Array Queue
 		assertTrue(queue.isEmpty());
 		queue.add(10);
 		assertFalse(queue.isEmpty());
+		
+		// LinkedQueue
+		assertTrue(queue2.isEmpty());
+		queue.add(10);
+		assertFalse(queue2.isEmpty());
 	}
 	
 	@Test
@@ -25,7 +33,9 @@ public class QueueTest {
 		
 		for (int n : data) {
 			queue.add(n);
+			queue2.add(n);
 			assertEquals(47, (int)queue.peek());
+			assertEquals(47, (int)queue2.peek());
 		}
 	
 	}
@@ -36,30 +46,41 @@ public class QueueTest {
 		
 		for (int n : data) {
 			queue.add(n);
+			queue2.add(n);
+			// Array Queue
 			assertFalse(queue.isEmpty());
 			assertEquals(n, (int)queue.remove());
 			assertTrue(queue.isEmpty());
+			// Linked Queue
+			assertFalse(queue2.isEmpty());
+			assertEquals(n, (int)queue2.remove());
+			assertTrue(queue2.isEmpty());
 		}		
 		
 		for (int n : data) {
 			queue.add(n);
-		}
-		for (int n : data) {
 			queue.add(n);
+			queue2.add(n);
+			queue2.add(n);
 		}
 		queue.remove();
 		queue.remove();
+		queue2.remove();
+		queue2.remove();
 		for (int n : data) {
 			queue.add(n);
+			queue2.add(n);
 		}
 		
 		for (int i = 2; i < data.length; i++) {
 			assertEquals(data[i], (int)queue.remove());
+			assertEquals(data[i], (int)queue2.remove());
 		}
 		for (int n : data) {
 			assertEquals(n, (int)queue.remove());
+			assertEquals(n, (int)queue2.remove());
 		}		
-		
+
 		while(!queue.isEmpty()) {
 			queue.remove();
 		}		
